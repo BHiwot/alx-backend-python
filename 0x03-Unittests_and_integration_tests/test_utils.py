35dea6bb2e2a93b5157fd8c384d3b290811aa207
Hiwot Berhanu <hmmmfb@gmail.com>
2023-11-13 04:39:04 -0800

    first commit

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Parameterize a unit test, Mock HTTP calls, Parameterize and patch """
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from utils import access_nested_map, get_json, memoize
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ TESTCASE """
+    """ to test the function for following inputs """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
